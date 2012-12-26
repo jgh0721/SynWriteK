e5f432eba6d52ba96a4ec5791bc70f759e247ec9
jgh0721 <jgh0721@gmail.com>
2012-12-26 14:46:12 +0900

    Initial Commit

---
+++ README.rst
@@ -0,0 +1,18 @@
+﻿
+Localization Environments
+==========================
+
+* Radialix 3.X Beta 
+
+* Glossary
+
+
+Version History
+==================
+
+
+2012-12-26
+--------------
+
+* SynWrite 4.6.550
+
---
+++ SynWrite/Template/newdoc/Delphi console app.pas
@@ -0,0 +1,27 @@
+{ *************************************************************************** }
+{                                                                             }
+{   Created by:                                                               }
+{   Short description                                                         }
+{   Last update:                                                 }
+{   Copyright ...                                                             }
+{                                                                             }
+{ *************************************************************************** }
+
+program MyProg;
+
+{$APPTYPE CONSOLE}
+
+uses Classes, Windows, SysUtils;
+
+{ variable definition here }
+var
+
+function Init()
+begin
+  { init kode here or delete it :-) }
+end;
+
+begin
+  Init;
+  { program code here }
+end;
